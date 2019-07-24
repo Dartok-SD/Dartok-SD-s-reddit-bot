e5b5afa0ab7242bedc7a0ba669becb35cf32d4db
David Sun <davidsun@umail.ucsb.edu>
2019-07-24 16:37:48 -0700

    Reads posts from subreddits
    Any post that is in the tracking list will then be printed

--- venv/RedditBot.py
+++ venv/RedditBot.py
@@ -1,5 +1,11 @@
 import SetUpPraw
+import sqlite3
+fileName = 'TestTracking.txt'
+subreddit = 'AskReddit'
 raw = SetUpPraw.setUp()
-sub = raw.subreddit('manga')
-for submission in sub.new():
-	print (submission.title)
+sub = raw.subreddit(subreddit)
+lineList = [line.rstrip('\n') for line in open(fileName)]
+for submission in sub.stream.submissions(skip_existing = True):
+	# print(submission.title)
+	if any(word in submission.title for word in lineList):
+		print(submission.title)
